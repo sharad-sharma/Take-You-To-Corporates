aef2ca85401a7cc6c5e6b3d2201f04d0598edff5
sharad-sharma <sharadsharma8959@gmail.com>
2021-11-12 10:35:18 +0530

    restructuring

--- Backend/config/keys.js
+++
@@ -1,8 +0,0 @@
-// add this file to .gitingonre
-
-module.exports = {
-    google: {
-        clientID: "339174328132-a1ru1dth62ja6voqjtt1g7vsai4fukhp.apps.googleusercontent.com",
-        clientSecret: "wbb7up2MrkeA4Kl2idamSEQE"
-    }
-};
--- Backend/server.js
+++ Backend/server.js
@@ -1,35 +1,3 @@
-
-const express = require('express');
-const passport = require('passport')
-var cookieParser = require('cookie-parser');
-
-const profileRoutes = require("./routes/profile-routes");
-const authRoutes = require("./routes/auth-routes");
-const passportSetup = require('./config/passport-setup')
-const session = require("express-session");
-
-const app = express();
-app.use(cookieParser());
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-// set cookie session 
-// TODO: Go through it again
-// app.use(cookieSession({
-//   maxAge: 24*60*60*1000,
-//   keys: [keys.session.cookieKey]
-// }))
-
-app.use(session({secret: "secret"}));
-// initialize passport
-app.use(passport.initialize());
-app.use(passport.session());
-
-// set up routes
-app.use("/auth", authRoutes);
-app.use("/profile", profileRoutes);
-=======
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db.js");
@@ -43,7 +11,6 @@ const userRoute = require("./routes/userRoutes");
 const companyRoute = require("./routes/companyRoutes");
 
 const app = express();
->>>>>>> 0c0aff52c5952dbb393369db6b409a7669602906
 
 app.use(express.json());
 app.use("/post", postRoute);
